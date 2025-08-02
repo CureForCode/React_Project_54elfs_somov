23084a663ebce01095b4d1d61aa1edafb797f0c8
CureForCode <artursomov@yahoo.de>
2025-08-02 21:35:04 +0200

    Lesson 3 was added. Whole React is updated. Button component was modified.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 // Lessons
+//import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
 
 // Homeworks
+//import Homework_02 from "./homeworks/Homework_02/Homework_02";
+
 import "./App.css";
-import Homework02 from "./homeworks/homework02/Homework02";
 
 function App() {
   return (
     <div className="app">
       {/* Lessons */}
+      {/* <Lesson_02 /> */}
+      <Lesson_03 />
       {/* Homeworks */}
-      <Homework02 />
+      {/* <Homework_02 /> */}
     </div>
   );
 }
